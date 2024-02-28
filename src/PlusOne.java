b72e58cd2bfc41533f00b7ba5df8262d9afb75e6
MatheusHenry2 <matheus.play7@live.com>
2024-02-27 23:43:31 -0300

    Plus One

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -4,35 +4,22 @@ public class PlusOne {
 
     public int[] plusOne(int[] digits) {
 
-        if (digits.length == 1) {
-            if (digits[0] == 9) {
-                return new int[]{1, 0};
-            } else {
-                return new int[]{digits[0] = digits[0] + 1};
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-        } else {
-            if (digits[digits.length - 1] == 9) {
-                int[] newArray = new int[digits.length + 1];
-                for (int i = 0; i < digits.length + 1; i++) {
-                    if (i == digits.length - 1) {
-                        newArray[i] = 1;
-                    } else if (i == digits.length) {
-                        newArray[i] = 0;
-                    } else {
-                        newArray[i] = digits[i];
-                    }
-                }
-            } else {
-                digits[digits.length - 1] = digits[digits.length - 1] + 1;
-            }
+            digits[i] = 0;
         }
 
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
 
-        return digits;
+        return newDigits;
     }
 
     public static void main(String[] args) {
-        int[] firstArray = {9};
+        int[] firstArray = {9, 9};
         PlusOne plusOne = new PlusOne();
         System.out.println(Arrays.toString(plusOne.plusOne(firstArray)));
     }
